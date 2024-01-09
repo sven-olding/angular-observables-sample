b0014ea993d7e49cb45ffa2ae347bcdc681f400b
Sven Olding <sven.olding@gmx.de>
2024-01-09 18:18:06 +0100

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+import { Observable } from 'rxjs-compat';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  intervalSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    // this.intervalSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.intervalSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed!');
+      }
+    );
+  }
+}
